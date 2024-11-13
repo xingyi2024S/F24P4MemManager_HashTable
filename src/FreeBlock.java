9040b0dfa6ae86bd28faf7c26e1db4fd557f5bde
Zhengyang Lu <zhengyang.lu.x@gmail.com>
2024-11-12 21:41:27 -0500

    Add Javadocs to FreeBlock

--- src/FreeBlock.java
+++ src/FreeBlock.java
@@ -1,8 +1,15 @@
+/**
+ * FreeBlock class.
+ * 
+ * @author Xingyi Wang
+ * @author Zhengyang Lu
+ * @version 2024.11.12
+ */
 public class FreeBlock {
-    int position;
-    int size;
-    FreeBlock next;
-    FreeBlock previous;
+    private int position;
+    private int size;
+    private FreeBlock next;
+    private FreeBlock previous;
 
     /**
      * Constructor to create a FreeBlock.
@@ -20,36 +27,76 @@ public class FreeBlock {
     }
 
 
-
+    /**
+     * Get position.
+     * 
+     * @return Position.
+     */
     public int getPosition() {
         return position;
     }
 
 
-  
+    /**
+     * Get size.
+     * 
+     * @return Size.
+     */
     public int getSize() {
         return size;
     }
 
 
+    /**
+     * Set size.
+     * 
+     * @param size
+     *            Size to set.
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+
+    /**
+     * Get the next FreeBlock.
+     * 
+     * @return The next FreeBlock.
+     */
     public FreeBlock getNext() {
         return next;
     }
 
+
+    /**
+     * Set the next FreeBlock.
+     * 
+     * @param next
+     *            The FreeBlock to set as next.
+     */
     public void setNext(FreeBlock next) {
         this.next = next;
     }
 
 
-
+    /**
+     * Get the previous FreeBlock.
+     * 
+     * @return The previous FreeBlock.
+     */
     public FreeBlock getPrevious() {
         return previous;
     }
 
 
-
+    /**
+     * Set the next FreeBlock.
+     * 
+     * @param previous
+     *            The FreeBlock to set as previous.
+     */
     public void setPrevious(FreeBlock previous) {
         this.previous = previous;
     }
-    
+
 }
